51adea5e760ae2b0c5ba82302cf40a3848e30b19
Tortilja <iljadevop@gmail.com>
2023-05-29 18:22:55 +0300

    FinishSixtHomeWorkV1

--- src/main/java/io/codelex/collections/practice/WordCount.java
+++ src/main/java/io/codelex/collections/practice/WordCount.java
@@ -10,9 +10,36 @@ import java.nio.file.Paths;
 public class WordCount {
     private static final Charset charset = Charset.defaultCharset();
     private static final String file = "/collections/lear.txt";
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        final Path path = Paths.get(Histogram.class.getResource(file).toURI());
-        Files.readAllLines(path, charset);
+
+        try {
+            Path path = Paths.get(WordCount.class.getResource(file).toURI());
+            String content = Files.readString(path, charset);
+
+            int lines = countLines(content);
+            int words = countWords(content);
+            int characters = countCharacters(content);
+
+            System.out.println("Lines = " + lines);
+            System.out.println("Words = " + words);
+            System.out.println("Chars = " + characters);
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int countLines(String content) {
+        String[] lines = content.split("\n");
+        return lines.length;
+    }
+
+    private static int countWords(String content) {
+        String[] words = content.split("\\s+");
+        return words.length;
+    }
+
+    private static int countCharacters(String content) {
+        return content.length();
     }
 }
